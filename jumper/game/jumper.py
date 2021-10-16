3c1a5fe00a4434e9f7a64ed11f993b4ee99c861f
neoBontia <bon21004@byui.edu>
2021-10-16 21:44:23 +0800

    Added jumper.py and state.py

---
+++ jumper/game/jumper.py
@@ -0,0 +1,16 @@
+class Jumper:
+    
+    def __init__(self) -> None:
+        pass
+
+    def guessALetter():
+        pass
+
+    def getGuess():
+        pass
+
+    def updateLives(boolean):
+        pass
+
+    def getLives():
+        pass
---
+++ jumper/game/state.py
@@ -0,0 +1,10 @@
+class State:
+
+    def __init__(self) -> None:
+        pass
+
+    def updateStatus(number, listOfChar):
+        pass
+
+    def draw():
+        pass
